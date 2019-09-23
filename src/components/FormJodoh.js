b764eb3c6ba712eea404ba3f096cffb2adb89b52
Cokde Susila <cokdes@gmail.com>
2019-09-23 23:48:22 +0800

    add form validation

--- src/components/FormJodoh.js
+++ src/components/FormJodoh.js
@@ -1,164 +1,293 @@
-import React, { Component } from "react";
+import React, {Component} from 'react';
 import {
   FormGroup,
   Label,
   Input,
   InputGroup,
   InputGroupAddon,
-  InputGroupText
-} from "reactstrap";
-import "./HomeStyle.css";
+  InputGroupText,
+  Button,
+  Form,
+  FormText,
+  FormFeedback,
+} from 'reactstrap';
+import './HomeStyle.css';
 
 export default class FormJodoh extends Component {
+  constructor(props) {
+    super(props);
+    this.handleButtonCek = this.handleButtonCek.bind(this);
+    this.handleButtonTry = this.handleButtonTry.bind(this);
+    this.state = {
+      wewaran_kamu: {},
+      wewaran_pasangan: {},
+      match: {},
+      isSubmitted: false,
+      email: '',
+      email_invalid: false,
+      namaanda: '',
+      namaanda_invalid: false,
+      tl1: '',
+      tl1_invalid: false,
+      namapasangan: '',
+      namapasangan_invalid: false,
+      tl2: '',
+      tl2_invalid: false,
+      formvalid: true,
+    };
+  }
+
+  handleButtonTry() {
+    this.setState({isSubmitted: false});
+  }
+
+  handleButtonCek = async event => {
+    event.preventDefault();
+    //alert(this.state.email.length);
+    if (this.state.email.length === 0) {
+      await this.setState({formvalid: false});
+      await this.setState({email_invalid: true});
+    } else if (this.state.namaanda.length === 0) {
+      await this.setState({formvalid: false});
+      await this.setState({namaanda_invalid: true});
+    }
+
+    this.state.formvalid
+      ? this.setState({isSubmitted: true})
+      : this.setState({isSubmitted: false});
+    // alert(this.state.isSubmitted);
+    // alert(
+    //   this.state.email +
+    //     this.state.namaanda +
+    //     this.state.tl1 +
+    //     this.state.namapasangan +
+    //     this.state.tl2
+    // );
+  };
+
+  changeHandler = event => {
+    this.setState({[event.target.name]: event.target.value});
+    this.setState({[`${event.target.name}_invalid`]: false});
+    this.setState({formvalid: true});
+  };
+
   render() {
+    const isSubmitted = this.state.isSubmitted;
     return (
       <div>
-        <h6 style={{ textAlign: "center" }}>
-          Cek <strong>kecocokan jodoh</strong> anda sekarang
-        </h6>
-        <FormGroup style={{ marginBottom: 6 }}>
-          <Label for="email" className="label">
-            Alamat Email Anda
-          </Label>
-          <InputGroup>
-            <Input
-              type="email"
-              name="email"
-              id="email"
-              placeholder="jayaprana@tresnan.co"
-              style={{
-                borderRadius: "10px 0 0 10px",
-                height: "38px",
-                fontSize: "0.85rem"
-              }}
-            />
-            <InputGroupAddon addonType="append">
-              <InputGroupText
-                style={{
-                  backgroundColor: "none",
-                  borderRadius: "0 10px 10px 0"
-                }}
-              >
-                <i class="material-icons md-18">email</i>
-              </InputGroupText>
-            </InputGroupAddon>
-          </InputGroup>
-        </FormGroup>
-        <FormGroup style={{ marginBottom: 6 }}>
-          <Label for="namaanda" className="label">
-            Nama Anda
-          </Label>
-          <InputGroup>
-            <Input
-              type="text"
-              name="namaanda"
-              id="namaanda"
-              placeholder="Mr./Mrs. Smith"
-              style={{
-                borderRadius: 10,
-                height: "38px",
-                fontSize: "0.85rem"
-              }}
-            />
-          </InputGroup>
-        </FormGroup>
-        <FormGroup style={{ marginBottom: 6 }}>
-          <Label for="tl2" className="label">
-            Tanggal Lahir Anda
-          </Label>
-          <InputGroup>
-            <Input
-              type="date"
-              name="tl1"
-              id="tl1"
-              placeholder="date placeholder"
-              style={{
-                borderRadius: "10px 0 0 10px",
-                height: "38px",
-                fontSize: "0.85rem"
-              }}
-            />
-            <InputGroupAddon addonType="append">
-              <InputGroupText
-                style={{
-                  backgroundColor: "none",
-                  borderRadius: "0 10px 10px 0"
-                }}
-              >
-                <i class="material-icons md-18">date_range</i>
-              </InputGroupText>
-            </InputGroupAddon>
-          </InputGroup>
-        </FormGroup>
-        <FormGroup style={{ marginBottom: 6 }}>
-          <Label for="namapasangan" className="label">
-            Nama Pasangan Anda
-          </Label>
-          <InputGroup>
-            <Input
-              type="text"
-              name="namapasangan"
-              id="namapasangan"
-              placeholder="Mr./Mrs. Smith"
-              style={{
-                borderRadius: 10,
-                height: "38px",
-                fontSize: "0.85rem"
-              }}
-            />
-          </InputGroup>
-        </FormGroup>
-        <FormGroup style={{ marginBottom: 6 }}>
-          <Label for="tl2" className="label">
-            Tanggal Lahir Pasangan Anda
-          </Label>
-          <InputGroup>
-            <Input
-              type="date"
-              name="tl2"
-              id="tl2"
-              placeholder="date placeholder"
-              style={{
-                borderRadius: "10px 0 0 10px",
-                height: "38px",
-                fontSize: "0.85rem"
-              }}
-            />
-            <InputGroupAddon addonType="append">
-              <InputGroupText
-                style={{
-                  backgroundColor: "none",
-                  borderRadius: "0 10px 10px 0"
-                }}
-              >
-                <i class="material-icons md-18">date_range</i>
-              </InputGroupText>
-            </InputGroupAddon>
-          </InputGroup>
-        </FormGroup>{" "}
-        <FormGroup style={{ fontSize: "0.85rem", width: "100%" }}>
-          <Label for="radio1" className="label">
-            Hubungan anda dengan pasangan
-          </Label>
-          <br />
+        {!isSubmitted ? (
+          <Form>
+            <h6 style={{textAlign: 'center'}}>
+              Cek <strong>kecocokan jodoh</strong> anda sekarang
+            </h6>
+            <FormGroup style={{marginBottom: 6}}>
+              <Label for="email" className="label">
+                Alamat Email Anda
+              </Label>
+              <InputGroup>
+                <Input
+                  type="email"
+                  name="email"
+                  id="email"
+                  placeholder="love@tresnan.co"
+                  style={{
+                    borderRadius: '10px 0 0 10px',
+                    height: '38px',
+                    fontSize: '0.75rem',
+                  }}
+                  value={this.state.email}
+                  onChange={this.changeHandler}
+                  invalid={this.state.email_invalid}
+                />
+                <InputGroupAddon addonType="append">
+                  <InputGroupText
+                    style={{
+                      backgroundColor: 'none',
+                      borderRadius: '0 10px 10px 0',
+                    }}
+                  >
+                    <i class="material-icons md-18">email</i>
+                  </InputGroupText>
+                </InputGroupAddon>
+                <FormFeedback className="formfeedback">
+                  Mohon mengisi email anda sesuai format baku (contoh:
+                  love@tresnan.co)
+                </FormFeedback>
+              </InputGroup>
+              <FormText style={{textAlign: 'right', fontSize: '0.65em'}}>
+                <i>Kami tidak akan membagikan email anda kepada siapapun</i>
+              </FormText>
+            </FormGroup>
+            <FormGroup style={{marginBottom: 6}}>
+              <Label for="namaanda" className="label">
+                Nama Anda
+              </Label>
+              <InputGroup>
+                <Input
+                  type="text"
+                  name="namaanda"
+                  id="namaanda"
+                  placeholder="Mr./Mrs. Smith"
+                  style={{
+                    borderRadius: 10,
+                    height: '38px',
+                    fontSize: '0.75em',
+                  }}
+                  value={this.state.namaanda}
+                  onChange={this.changeHandler}
+                  invalid={this.state.namaanda_invalid}
+                />
+                <FormFeedback className="formfeedback">
+                  Mohon mengisi nama anda
+                </FormFeedback>
+              </InputGroup>
+            </FormGroup>
+            <FormGroup style={{marginBottom: 6}}>
+              <Label for="tl2" className="label">
+                Tanggal Lahir Anda
+              </Label>
+              <InputGroup>
+                <Input
+                  type="date"
+                  name="tl1"
+                  id="tl1"
+                  placeholder="date placeholder"
+                  style={{
+                    borderRadius: '10px 0 0 10px',
+                    height: '38px',
+                    fontSize: '0.75em',
+                  }}
+                  value={this.state.tl1}
+                  onChange={this.changeHandler}
+                  invalid={this.state.tl1_invalid}
+                />
+                <InputGroupAddon addonType="append">
+                  <InputGroupText
+                    style={{
+                      backgroundColor: 'none',
+                      borderRadius: '0 10px 10px 0',
+                    }}
+                  >
+                    <i class="material-icons md-18">date_range</i>
+                  </InputGroupText>
+                </InputGroupAddon>
+                <FormFeedback className="formfeedback">
+                  Mohon mengisi tanggal lahir anda
+                </FormFeedback>
+              </InputGroup>
+            </FormGroup>
+            <FormGroup style={{marginBottom: 6}}>
+              <Label for="namapasangan" className="label">
+                Nama Pasangan Anda
+              </Label>
+              <InputGroup>
+                <Input
+                  type="text"
+                  name="namapasangan"
+                  id="namapasangan"
+                  placeholder="Mr./Mrs. Smith"
+                  style={{
+                    borderRadius: 10,
+                    height: '38px',
+                    fontSize: '0.75em',
+                  }}
+                  value={this.state.namapasangan}
+                  onChange={this.changeHandler}
+                  invalid={this.state.namapasangan_invalid}
+                />
+                <FormFeedback className="formfeedback">
+                  Mohon mengisi nama pasangan anda
+                </FormFeedback>
+              </InputGroup>
+            </FormGroup>
+            <FormGroup style={{marginBottom: 6}}>
+              <Label for="tl2" className="label">
+                Tanggal Lahir Pasangan Anda
+              </Label>
+              <InputGroup>
+                <Input
+                  type="date"
+                  name="tl2"
+                  id="tl2"
+                  placeholder="date placeholder"
+                  style={{
+                    borderRadius: '10px 0 0 10px',
+                    height: '38px',
+                    fontSize: '0.75em',
+                  }}
+                  value={this.state.tl2}
+                  onChange={this.changeHandler}
+                  invalid={this.state.tl2_invalid}
+                />
+                <InputGroupAddon addonType="append">
+                  <InputGroupText
+                    style={{
+                      backgroundColor: 'none',
+                      borderRadius: '0 10px 10px 0',
+                    }}
+                  >
+                    <i class="material-icons md-18">date_range</i>
+                  </InputGroupText>
+                </InputGroupAddon>
+                <FormFeedback className="formfeedback">
+                  Mohon mengisi tanggal lahir pasangan anda
+                </FormFeedback>
+              </InputGroup>
+            </FormGroup>{' '}
+            {/* <FormGroup style={{fontSize: '0.75em', width: '100%'}}>
+              <Label for="radio1" className="label">
+                Hubungan anda dengan pasangan
+              </Label>
+              <br />
 
-          <FormGroup check inline style={{ marginLeft: 10 }}>
-            <Label check>
-              <Input type="radio" name="radio1" /> PDKT
-            </Label>
-          </FormGroup>
-          <FormGroup check inline>
-            <Label check>
-              <Input type="radio" name="radio1" /> Pacaran
-            </Label>
-          </FormGroup>
-          <FormGroup check inline>
-            <Label check>
-              <Input type="radio" name="radio1" /> Menikah
-            </Label>
-          </FormGroup>
-        </FormGroup>
-        <br />
+              <FormGroup check inline style={{marginLeft: 10}}>
+                <Label check>
+                  <Input type="radio" id="hubungan" name="radio1" /> PDKT
+                </Label>
+              </FormGroup>
+              <FormGroup check inline>
+                <Label check>
+                  <Input type="radio" id="hubungan" name="radio1" /> Pacaran
+                </Label>
+              </FormGroup>
+              <FormGroup check inline>
+                <Label check>
+                  <Input type="radio" id="hubungan" name="radio1" /> Menikah
+                </Label>
+              </FormGroup>
+            </FormGroup> */}
+            <Button
+              style={{
+                backgroundColor: '#FC5185',
+                borderRadius: 10,
+                border: 'none',
+                boxShadow: '0 2px 2px grey',
+                padding: '12px 30px 12px 30px',
+                margin: '10px 0 0 0',
+              }}
+              onClick={this.handleButtonCek}
+            >
+              Cek Sekarang >>
+            </Button>
+          </Form>
+        ) : (
+          <div>
+            haloha
+            <br />
+            <Button
+              style={{
+                backgroundColor: '#FC5185',
+                borderRadius: 10,
+                border: 'none',
+                boxShadow: '0 2px 2px grey',
+                padding: '12px 30px 12px 30px',
+              }}
+              onClick={this.handleButtonTry}
+            >
+              Coba Lagi
+            </Button>
+          </div>
+        )}
       </div>
     );
   }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,26 +1,15 @@
-import React, { Component } from "react";
-import {
-  Container,
-  Navbar,
-  NavbarBrand,
-  Row,
-  Col,
-  Form,
-  Button
-} from "reactstrap";
-import FormJodoh from "./FormJodoh";
-import axios from "axios";
+import React, {Component} from 'react';
+import {Container, Navbar, NavbarBrand, Row, Col} from 'reactstrap';
+import FormJodoh from './FormJodoh';
+import axios from 'axios';
 
 export default class Home extends Component {
   constructor(props) {
     super(props);
-    this.handleButtonCek = this.handleButtonCek.bind(this);
-    this.handleButtonTry = this.handleButtonTry.bind(this);
     this.state = {
       wewaran_kamu: {},
       wewaran_pasangan: {},
       match: {},
-      isSubmitted: false
     };
   }
 
@@ -29,16 +18,16 @@ export default class Home extends Component {
     // then set them in state
     try {
       const params = {
-        tl1: "1985-03-25T00:00:00Z",
-        tl2: "1985-07-28T00:00:00Z"
+        tl1: '1985-03-25T00:00:00Z',
+        tl2: '1985-07-28T00:00:00Z',
       };
       const res = await axios.post(
-        "https://v6e49pog65.execute-api.ap-southeast-1.amazonaws.com/prod/tripremana/",
+        'https://v6e49pog65.execute-api.ap-southeast-1.amazonaws.com/prod/tripremana/',
         params
       );
-      this.setState({ wewaran_kamu: res.data.wewaran_kamu });
-      this.setState({ wewaran_pasangan: res.data.wewaran_pasangan });
-      this.setState({ match: res.data.hasil_tripremana });
+      this.setState({wewaran_kamu: res.data.wewaran_kamu});
+      this.setState({wewaran_pasangan: res.data.wewaran_pasangan});
+      this.setState({match: res.data.hasil_tripremana});
     } catch (err) {
       console.log(`An Error has occured: ${err}`);
     }
@@ -48,90 +37,45 @@ export default class Home extends Component {
     this.fetchProducts();
   };
 
-  handleButtonTry() {
-    this.setState({ isSubmitted: false });
-  }
-
-  handleButtonCek() {
-    this.setState({ isSubmitted: true });
-  }
-
   render() {
-    const isSubmitted = this.state.isSubmitted;
-
     return (
-      <div style={{ fontFamily: '"Poppins", sans-serif' }}>
+      <div style={{fontFamily: '"Poppins", sans-serif'}}>
         <Navbar color="faded" light expand="md">
           <NavbarBrand href="/">
-            <img src="logo_tresnan.png" alt="Logo" width="100" />
+            <img src="logo_tresnan.png" alt="Logo" width="120" />
           </NavbarBrand>
         </Navbar>
         <Container>
-          <br />
           <Row>
             <Col md="5">
-              <h5 style={{ color: "#364F6B", fontSize: "1.4em" }}>
-                Karena <strong>hidup</strong> anda begitu{" "}
-                <strong>berharga</strong>
+              <h5 style={{color: '#364F6B', fontSize: '1.3em'}}>
+                Karena <span style={{fontWeight: '700'}}>hidup</span> anda
+                begitu <strong>berharga</strong>
               </h5>
-              <p style={{ color: "#dd8218" }}>
+              <p style={{color: '#dd8218'}}>
                 Pilihlah <strong>jodoh terbaik untuk menjalaninya</strong>
               </p>
               <div
                 style={{
-                  backgroundColor: "#f5f5f5",
+                  backgroundColor: '#f5f5f5',
                   borderRadius: 15,
                   padding: 12,
-                  boxShadow: "0px 2px 4px grey"
+                  margin: 0,
+                  boxShadow: '0px 2px 4px grey',
                 }}
               >
-                <Form>
-                  {isSubmitted ? (
-                    <div>
-                      Mantapp
-                      <br />
-                      <Button
-                        style={{
-                          backgroundColor: "#FC5185",
-                          borderRadius: 10,
-                          border: "none",
-                          boxShadow: "0 2px 2px grey",
-                          padding: "12px 30px 12px 30px"
-                        }}
-                        onClick={this.handleButtonTry}
-                      >
-                        Coba Lagi
-                      </Button>
-                    </div>
-                  ) : (
-                    <div>
-                      <FormJodoh />
-                      <Button
-                        style={{
-                          backgroundColor: "#FC5185",
-                          borderRadius: 10,
-                          border: "none",
-                          boxShadow: "0 2px 2px grey",
-                          padding: "12px 30px 12px 30px"
-                        }}
-                        onClick={this.handleButtonCek}
-                      >
-                        Cek Sekarang >>
-                      </Button>
-                    </div>
-                  )}
+                <FormJodoh />
 
-                  <br />
-                  <p style={{ fontSize: "0.8rem" }}>
-                    *) Perhitungan kecocokan jodoh ini adalah berdasarkan
-                    warisan budaya nusantara yang tertuang di dalam lontar Tri
-                    Premana yaitu pertemuan hari kelahiran pasangan. Pelajari
-                    lebih lanjut <a href="/">disini</a>.
-                  </p>
-                </Form>
+                <br />
+                <p style={{fontSize: '0.7rem'}}>
+                  *) Perhitungan kecocokan jodoh ini adalah berdasarkan warisan
+                  budaya nusantara yang tertuang di dalam lontar Tri Premana
+                  yang menghitung kombinasi pertemuan hari kelahiran pasangan.
+                  Pelajari lebih lanjut <a href="/">disini</a>.
+                </p>
               </div>
               <div>
-                Kamu lahir pada hari : {this.state.wewaran_kamu.saptawara},{" "}
+                Kamu lahir pada hari : {this.state.wewaran_kamu.saptawara},{' '}
                 {this.state.wewaran_kamu.pancawara}
                 <br />
                 {this.state.match.level}
